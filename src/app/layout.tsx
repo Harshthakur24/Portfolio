27f16f1b88e4886e6bd841d23e98eeb698b19eda
Harsh Thakur <109977809+Harshthakur24@users.noreply.github.com>
2024-06-02 02:40:51 +0530

    Name change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Personal Protfilio",
+  title: "Harsh Protfilio",
   description: "Developed by Harsh",
 };
 
