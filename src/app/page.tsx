725804e9f3deb4aea83027acdc52fad624c98e5b
Harsh Thakur <109977809+Harshthakur24@users.noreply.github.com>
2024-06-03 13:59:12 +0530

    Added Footer and Skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ import HeroSection from "@/components/HeroSection";
 import Footer from "@/components/Footer";
 import AboutSection from "@/components/AboutSection";
 import Navbar from "@/components/Navbar";
+import { Skills } from "@/components/Skills";
+
+
+
 
 export default function Home() {
 
@@ -10,11 +14,9 @@ export default function Home() {
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar/>
       <div className="container mt-24 mx-auto px-12 py-4">
-      
         <HeroSection />
         <AboutSection />
-        
-
+        <Skills/>
         
       </div>
       <Footer/>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+"use client";
+import React from 'react';
+import { FaTwitter, FaInstagram, FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
-const Footer = () => {
+const Footer: React.FC = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
-        <span>Harsh</span>
-        <p className="text-slate-600">All rights reserved.</p>
+    <footer className="bg-black text-white py-8 mt-auto">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-2 gap-4">
+          <div className="flex flex-col">
+            <p className="text-lg font-bold mb-4">Find and connect me on:</p>
+            <div className="flex space-x-4">
+              <button onClick={() => window.open('https://x.com/HarshThaku44904', '_blank')} className="text-blue-400 hover:text-blue-500 transition duration-300 flex items-center">
+                <FaTwitter className="mr-2" /> Twitter
+              </button>
+              <button onClick={() => window.open('https://www.instagram.com/thakurharsh.24/', '_blank')} className="text-blue-400 hover:text-blue-500 transition duration-300 flex items-center">
+                <FaInstagram className="mr-2" /> Instagram
+              </button>
+              <button onClick={() => window.open('https://github.com/Harshthakur24', '_blank')} className="text-blue-400 hover:text-blue-500 transition duration-300 flex items-center">
+                <FaGithub className="mr-2" /> GitHub
+              </button>
+              <button onClick={() => window.open('https://www.linkedin.com/in/harsh-thakur-20877b246/', '_blank')} className="text-blue-400 hover:text-blue-500 transition duration-300 flex items-center">
+                <FaLinkedinIn className="mr-2" /> LinkedIn
+              </button>
+            </div>
+          </div>
+          <div className="flex items-end justify-end">
+            <p className="text-sm">© 2024 Harsh Thakur. All rights reserved.</p>
+          </div>
+        </div>
       </div>
     </footer>
   );
