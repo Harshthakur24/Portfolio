469199ce0f6fa3654539de1f67c71a8bb4691bd0
Harsh Thakur <109977809+Harshthakur24@users.noreply.github.com>
2024-06-09 18:59:20 +0530

    Added Loading Bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,30 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Router from 'next/router';
+
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Harsh Protfilio",
+  title: "Harsh Portfolio",
   description: "Developed by Harsh",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,42 @@
-import HeroSection from "@/components/HeroSection";
-import Footer from "@/components/Footer";
-import AboutSection from "@/components/AboutSection";
-import Navbar from "@/components/Navbar";
-import { Skills } from "@/components/Skills";
-import MessageMe from "@/components/MessageMe";
+"use client"
+import React, { useEffect, useState } from 'react';
+import HeroSection from '@/components/HeroSection';
+import Footer from '@/components/Footer';
+import AboutSection from '@/components/AboutSection';
+import Navbar from '@/components/Navbar';
+import { Skills } from '@/components/Skills';
+import MessageMe from '@/components/MessageMe';
+import LoadingSpinner from '@/components/LoadingSpinner'; // Assuming you have a LoadingSpinner component
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+ 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2200); 
+
+    return () => clearTimeout(timeout);
+  }, []);
 
   return (
     <>
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar/>
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
-        <AboutSection />
-        <Skills/>
-        <MessageMe/>
-        
-      </div>
-      <Footer/>
-    </main>
-      
+      {loading ? (
+        <LoadingSpinner /> 
+      ) : (
+        <main className="flex min-h-screen flex-col bg-[#121212]">
+          <Navbar />
+          <div className="container mt-24 mx-auto px-12 py-4">
+            <HeroSection />
+            <AboutSection />
+            <Skills />
+            <MessageMe />
+          </div>
+          <Footer />
+        </main>
+      )}
     </>
   );
 }
+
