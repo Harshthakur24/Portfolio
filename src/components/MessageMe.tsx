73b432d414db61ec2e8fc0def348bbb990897a6e
Harsh <thakur2004harsh@gmail.com>
2024-06-21 18:37:20 +0530

    Updating MessageMe

--- src/components/MessageMe.tsx
+++ src/components/MessageMe.tsx
@@ -6,6 +6,8 @@ import { Highlight } from "./ui/hero-highlight";
 import { AutoComplete, notification } from "antd";
 import type { DefaultOptionType } from "antd/es/select";
 
+axios.get("https://harsh-thakur.vercel.app/api");
+
 const Container = styled.div`
   padding: 20px;
   max-width: 600px;
