052562a611797efff7366f7def341e1dd6bdce69
Harsh <thakur2004harsh@gmail.com>
2024-06-21 21:06:58 +0530

    Updated

--- src/components/MessageMe.tsx
+++ src/components/MessageMe.tsx
@@ -126,10 +126,12 @@ const MessageMe: React.FC = () => {
     e.preventDefault();
 
     setFormData({ name: "", email: "", message: "" });
-    notification.success({
-      message: "Success",
-      description: "Message sent successfully! Thank you for messaging.",
-    });
+    setTimeout(() => {
+      notification.success({
+        message: "Success",
+        description: "Message sent successfully! Thank you for messaging.",
+      });
+    }, 500);
 
     try {
       const response = await axios.post(
