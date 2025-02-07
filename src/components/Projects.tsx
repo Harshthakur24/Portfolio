2d862cb04b2f45dbf3e64e5de972a770c21b1648
Harsh <thakur2004harsh@gmail.com>
2025-02-07 19:17:49 +0530

    Project Added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -63,7 +63,7 @@ export default function Projects() {
                 </div>
                 <div className="p-4">
                   <h2 className="font-bold my-4 text-lg text-zinc-700 flex justify-between items-center">
-                    Nutriwise-AI <span>⭐⭐⭐⭐⭐</span>
+                    Nutriwise-AI <span>⭐⭐⭐⭐⭐⭐</span>
                   </h2>
                   <h2 className="font-normal my-4 text-sm text-zinc-500">
                     NutriEase is an AI-powered website designed to simplify personalized nutrition by offering tailored meal plans, calorie tracking, and interactive dashboards.
@@ -119,7 +119,7 @@ export default function Projects() {
                 </div>
                 <div className="p-4">
                   <h2 className="font-bold my-4 text-lg text-zinc-700 flex justify-between items-center">
-                    DitMyBit<span>⭐⭐⭐⭐</span>
+                    DitMyBit<span>⭐⭐⭐⭐⭐</span>
                   </h2>
                   <h2 className="font-normal my-4 text-sm text-zinc-500">
                     This is webiste that helps people finding the Social Organization and NGO&apos;s in thier neighbour as well as all over the World and help them by providing support.
@@ -469,19 +469,74 @@ export default function Projects() {
                 </div>
                 <div className="p-4">
                   <h2 className="font-bold my-4 text-lg text-zinc-700 flex justify-between items-center">
-                    Black Mining<span>⭐⭐⭐⭐⭐</span>
+                    Black Mining<span>⭐⭐⭐⭐</span>
                   </h2>
                   <h2 className="font-normal my-4 text-sm text-zinc-500">
                     Designed and implemented a professional, user-centric website to streamline car scrapping services for end-of-life vehicles, ensuring compliance with government standards and environmental regulations.
                   </h2>
                   <div className="flex flex-row justify-between items-center mt-10">
-                    <span className="text-sm text-gray-500">2024</span>
+                    <span className="text-sm text-gray-500">2025</span>
 
                     <motion.div whileHover={{ scale: 1.2 }}>
                       <a
                         onClick={() => {
                           window.open(
                             "https://black-mining-portfolio.vercel.app/",
+                            "_blank"
+                          );
+                        }}
+                        className="relative z-10 px-6 py-2 bg-black text-white font-bold rounded-xl block text-xs"
+                      >
+                        Visit it
+                      </a>
+                    </motion.div>
+                  </div>
+                </div>
+              </div>
+            </FollowerPointerCard>
+          </div>
+
+          {/* Project 9 */}
+          <div className="w-full sm:w-80 mx-auto">
+            <FollowerPointerCard
+              title={
+                <div className="flex space-x-2 items-center">
+                  <Image
+                    src="/logo.png"
+                    height="20"
+                    width="20"
+                    alt="thumbnail"
+                    className="rounded-full border-2 border-white"
+                  />
+                  <p>I build this website for my college.</p>
+                </div>
+              }
+            >
+              <div className="relative overflow-hidden h-full rounded-2xl transition duration-200 group bg-white hover:shadow-xl border border-zinc-100">
+                <div className="w-full aspect-w-16 aspect-h-9 bg-gray-100 rounded-tr-lg rounded-tl-lg overflow-hidden relative">
+                  <Image
+                    src="/mru-medical.jpg"
+                    alt="thumbnail"
+                    height="600"
+                    width="1200"
+                    className="group-hover:scale-95 group-hover:rounded-2xl transform object-cover transition duration-200"
+                  />
+                </div>
+                <div className="p-4">
+                  <h2 className="font-bold my-4 text-lg text-zinc-700 flex justify-between items-center">
+                    Black Mining<span>⭐⭐⭐⭐⭐</span>
+                  </h2>
+                  <h2 className="font-normal my-4 text-sm text-zinc-500">
+                    This is my college (MRU) Medical Portal is designed to streamline the medical certificate submission process for students. I had the opportunity to build this website for my college.
+                  </h2>
+                  <div className="flex flex-row justify-between items-center mt-10">
+                    <span className="text-sm text-gray-500">2025</span>
+
+                    <motion.div whileHover={{ scale: 1.2 }}>
+                      <a
+                        onClick={() => {
+                          window.open(
+                            "https://mr-umedicalportal.vercel.app/",
                             "_blank"
                           );
                         }}
